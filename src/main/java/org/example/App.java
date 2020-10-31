7e7c6e0987aaad2f68fb437de95e6243cbafa61a
Andris Taubers <andris@taubers.lv>
2020-10-31 11:30:26 +0200

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,31 @@
+// The purpose of the App is to teach to calculate numbers in the head - both for kids and adults.
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        //JOptionPane pane = new JOptionPane();
+        Random rand = new Random();
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+
+        int first_number = rand.nextInt(1000);
+        int second_number = rand.nextInt(1000);
+        int result = first_number + second_number;
+
+        System.out.println("Apēķini:\n");
+        System.out.println(first_number + " + " + second_number + " = " + "X\n");
+        System.out.println("Cik ir X?");
+
+        int answer = Integer.parseInt(br.readLine());
+
+        while (answer != result) {
+            System.out.println("Nav labi! Mēģini vēlreiz! \n" + "Cik ir " + first_number + " + " + second_number + " ?\n");
+            answer = Integer.parseInt(br.readLine());
+        }
+        System.out.println("Nu, ta beidzot!!! :)");
+    }
+}
