c35522bb350031aadcaad06671144e159a260aa4
Andris Taubers <andris@taubers.lv>
2021-04-08 13:08:34 +0300

    Added values to ExerciseParams in ExerciseFactory

--- src/main/java/exercises/ExerciseFactory.java
+++ src/main/java/exercises/ExerciseFactory.java
@@ -5,7 +5,6 @@ import java.security.SecureRandom;
 
 public class ExerciseFactory {
 
-
     public static AbstractExercise getExercise(Level level) {
         switch (level){
             case LEVEL_1 -> {
@@ -127,23 +126,133 @@ public class ExerciseFactory {
     }
 
     private static AbstractExercise createAdditionExercise(Level level) {
-        //TODO create ExerciseParams according to level
-        return new AdditionExercise(new ExerciseParams(1,10));
+        switch (level){
+            case LEVEL_1, LEVEL_2 -> {
+                ExerciseParams exerciseParams = new ExerciseParams(2,10, 2, 10);
+                return new AdditionExercise(exerciseParams);
+            }
+            case LEVEL_3 -> {
+                ExerciseParams exerciseParams = new ExerciseParams(20,900, 20, 900);
+                return new AdditionExercise(exerciseParams);
+            }
+            case LEVEL_4 -> {
+                ExerciseParams exerciseParams = new ExerciseParams(15,900, 15, 900);
+                return new AdditionExercise(exerciseParams);
+            }
+            case LEVEL_5 -> {
+                ExerciseParams exerciseParams = new ExerciseParams(120,900, 120, 900);
+                return new AdditionExercise(exerciseParams);
+            }
+            case LEVEL_6 -> {
+                ExerciseParams exerciseParams = new ExerciseParams(300,1200, 300, 1200);
+                return new AdditionExercise(exerciseParams);
+            }
+            case LEVEL_7 -> {
+                ExerciseParams exerciseParams = new ExerciseParams(300,1500, 300, 1200);
+                return new AdditionExercise(exerciseParams);
+            }
+            default -> {
+                throw new IllegalStateException();
+            }
+        }
+
     }
 
     private static AbstractExercise createSubtractionExercise(Level level) {
-        //TODO create ExerciseParams according to level
-        return new SubtractionExercise(new ExerciseParams(1,10));
+        switch (level){
+            case LEVEL_1, LEVEL_2 -> {
+                ExerciseParams exerciseParams = new ExerciseParams(2,10, 2, 10);
+                return new SubtractionExercise(exerciseParams);
+            }
+            case LEVEL_3 -> {
+                ExerciseParams exerciseParams = new ExerciseParams(50,900, 12, 849);
+                return new SubtractionExercise(exerciseParams);
+            }
+            case LEVEL_4 -> {
+                ExerciseParams exerciseParams = new ExerciseParams(200,900, 200, 900);
+                return new SubtractionExercise(exerciseParams);
+            }
+            case LEVEL_5 -> {
+                ExerciseParams exerciseParams = new ExerciseParams(300,900, 300, 900);
+                return new SubtractionExercise(exerciseParams);
+            }
+            case LEVEL_6 -> {
+                ExerciseParams exerciseParams = new ExerciseParams(600,1800, 600, 1800);
+                return new SubtractionExercise(exerciseParams);
+            }
+            case LEVEL_7 -> {
+                ExerciseParams exerciseParams = new ExerciseParams(600,1900, 600, 1800);
+                return new SubtractionExercise(exerciseParams);
+            }
+            default -> {
+                throw new IllegalStateException();
+            }
+        }
     }
 
     private static AbstractExercise createDivisionExercise(Level level) {
-        //TODO create ExerciseParams according to level
-        return new DivisionExercise(new ExerciseParams(1,10));
+        switch (level){
+            case LEVEL_2 -> {
+                ExerciseParams exerciseParams = new ExerciseParams(2,10, 2, 10);
+                return new DivisionExercise(exerciseParams);
+            }
+            case LEVEL_3 -> {
+                ExerciseParams exerciseParams = new ExerciseParams(80,200, 2, 15);
+                return new DivisionExercise(exerciseParams);
+            }
+            case LEVEL_4 -> {
+                ExerciseParams exerciseParams = new ExerciseParams(80,200, 2, 20);
+                return new DivisionExercise(exerciseParams);
+            }
+            case LEVEL_5 -> {
+                ExerciseParams exerciseParams = new ExerciseParams(150,300, 2, 25);
+                return new DivisionExercise(exerciseParams);
+            }
+            case LEVEL_6 -> {
+                ExerciseParams exerciseParams = new ExerciseParams(190,900, 4, 32);
+                return new DivisionExercise(exerciseParams);
+            }
+            case LEVEL_7 -> {
+                ExerciseParams exerciseParams = new ExerciseParams(190,900, 4, 35);
+                return new DivisionExercise(exerciseParams);
+            }
+            default -> {
+                throw new IllegalStateException();
+            }
+        }
+
     }
 
     private static AbstractExercise createMultiplicationExercise(Level level) {
-        //TODO create ExerciseParams according to level
-        return new MultiplicationExercise(new ExerciseParams(1,10));
+        switch (level){
+            case LEVEL_2 -> {
+                ExerciseParams exerciseParams = new ExerciseParams(2,10, 2, 10);
+                return new MultiplicationExercise(exerciseParams);
+            }
+            case LEVEL_3 -> {
+                ExerciseParams exerciseParams = new ExerciseParams(5,20, 12, 20);
+                return new MultiplicationExercise(exerciseParams);
+            }
+            case LEVEL_4 -> {
+                ExerciseParams exerciseParams = new ExerciseParams(5,25, 12, 20);
+                return new MultiplicationExercise(exerciseParams);
+            }
+            case LEVEL_5 -> {
+                ExerciseParams exerciseParams = new ExerciseParams(11,25, 12, 25);
+                return new MultiplicationExercise(exerciseParams);
+            }
+            case LEVEL_6 -> {
+                ExerciseParams exerciseParams = new ExerciseParams(15,30, 12, 30);
+                return new MultiplicationExercise(exerciseParams);
+            }
+            case LEVEL_7 -> {
+                ExerciseParams exerciseParams = new ExerciseParams(15,35, 12, 30);
+                return new MultiplicationExercise(exerciseParams);
+            }
+            default -> {
+                throw new IllegalStateException();
+            }
+        }
     }
 
 
--- src/main/java/exercises/ExerciseParams.java
+++ src/main/java/exercises/ExerciseParams.java
@@ -1,15 +1,16 @@
 package exercises;
 
 public class ExerciseParams {
-    protected int min1 = 2;
-    protected int max1 = 100;
-    //protected int min2 = 4;
-    //protected int max2 = 200;
+    private int min1;
+    private int max1;
+    private int min2;
+    private int max2;
 
-    public ExerciseParams(int min1, int max1/*, int min2, int max2*/) {
+
+    public ExerciseParams(int min1, int max1, int min2, int max2) {
         this.min1 = min1;
         this.max1 = max1;
-       // this.min2 = min2;
-        //this.max2 = max2;
+        this.min2 = min2;
+        this.max2 = max2;
     }
 }
