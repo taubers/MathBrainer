686124e797b2ea60f40cef5baa733c2e73c9a682
Andris Taubers <andris@taubers.lv>
2021-04-12 22:26:03 +0300

    Added ExerciseParams

--- src/main/java/exercises/SimplifiedDivisionExercise.java
+++ src/main/java/exercises/SimplifiedDivisionExercise.java
@@ -25,5 +25,6 @@ public class SimplifiedDivisionExercise extends AbstractExercise {
     public void calculateExpectedResult() {
         result = getFirstNumber() / getSecondNumber();
     }
+    //
 
 }
